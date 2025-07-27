91c749bbdbf9468cecffa0e1ff624b02f8735949
conadsciacca <conadsciaccamkt@gmail.com>
2025-07-27 23:48:47 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ def export_csv():
     )
 
 # ---------------- MAIN ----------------
+init_db()
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get("PORT", 5050))
     app.run(host='0.0.0.0', port=port)
