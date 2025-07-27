8d5a4efb0291ef4a426c3b4c11e114b95fc5340c
conadsciacca <conadsciaccamkt@gmail.com>
2025-07-27 21:19:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -72,8 +72,7 @@ def init_db():
 def get_dipendenti(store_id):
     with sqlite3.connect(DB) as conn:
         c = conn.cursor()
-        # Ora ritorna 4 colonne: id, nome, foto, store_id
-        c.execute("SELECT id, nome, foto, store_id FROM dipendenti WHERE store_id=?", (store_id,))
+        c.execute("SELECT id, nome, foto FROM dipendenti WHERE store_id=?", (store_id,))
         return c.fetchall()
 
 def allowed_file(filename):
